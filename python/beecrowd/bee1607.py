77c5b4a4c2bd792ecc3623040d6ec7d7014e60ea
Fernando Ghinzelli <fghinzelli@gmail.com>
2023-08-22 09:46:51 -0300

    bee1607

--- python/beecrowd/bee1607.py
+++ python/beecrowd/bee1607.py
@@ -1,16 +1,17 @@
 import string
 qtd_testes = int(input())
-string1, string2 = input().split()
 
 alphabet = list(string.ascii_lowercase)
 
-total_operations = 0
-for i in range(len(string1)):
-  index_string1 = alphabet.index(string1[i]) + 1
-  index_string2 = alphabet.index(string2[i]) + 1
-  if index_string1 < index_string2:
-    total_operations += (index_string2 - index_string1)
-  elif index_string1 > index_string2:
-    total_operations += ((len(alphabet) - index_string1) + index_string2)
+for i in range(0, qtd_testes):
+  total_operations = 0
+  string1, string2 = input().split()
+  for i in range(len(string1)):
+    index_string1 = alphabet.index(string1[i]) + 1
+    index_string2 = alphabet.index(string2[i]) + 1
+    if index_string1 < index_string2:
+      total_operations += (index_string2 - index_string1)
+    elif index_string1 > index_string2:
+      total_operations += ((len(alphabet) - index_string1) + index_string2)
+  print(total_operations)
 
-print(total_operations)
